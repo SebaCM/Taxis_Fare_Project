884c446681046b71f9fdbb0b7d5798ab2dc01e8a
Mauricio andres varela saenz <105470929+Mauriciodres@users.noreply.github.com>
2025-02-28 11:20:50 -0500

    Add files via upload
    changes were applied to implement the google api in html services and we limited the area to only new york

--- src/request_api.py
+++ src/request_api.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-import requests
+import httpx
 
 app = FastAPI()
 
 # Reemplaza con tu clave de API de Google Maps
-API_KEY = "AIzaSyC8jdYcvQaFHdh0VvXtkZWz2t8PsH3hAnA"
+API_KEY = "AIzaSyDnhBrF6rtcc0VS066RFF012bBd0sRJAJU"
 
 class RouteRequest(BaseModel):
     origin: str
@@ -16,12 +16,13 @@ async def calculate_route(request: RouteRequest):
     origin = request.origin
     destination = request.destination
 
-    url = f'https://routes.googleapis.com/directions/v2:computeRoutes'
-   # f"https://maps.googleapis.com/maps/api/directions/json?origin={origin}&destination={destination}&key={API_KEY}"
-    response = requests.get(url)
-    route_data = response.json()
+    url = f"https://maps.googleapis.com/maps/api/directions/json?origin={origin}&destination={destination}&key={API_KEY}"
 
-    if route_data['status'] == 'OK':
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url)
+        route_data = response.json()
+
+    if 'status' in route_data and route_data['status'] == 'OK':
         # Extrae la información relevante de la respuesta de la API
         legs = route_data['routes'][0]['legs']
         distance = legs[0]['distance']['text']
@@ -29,4 +30,5 @@ async def calculate_route(request: RouteRequest):
 
         return {"distance": distance, "duration": duration}
     else:
-        raise HTTPException(status_code=500, detail=route_data['status'])
+        error_message = route_data.get('error_message', 'Unknown error')
+        raise HTTPException(status_code=500, detail=error_message)
